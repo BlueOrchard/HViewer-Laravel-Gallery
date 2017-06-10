563818c28cde782fa9d99be93f0a2fb8db744dad
BlueOrchard <josephrichey@me.com>
2017-06-10 16:17:33 -0400

    Replaced DB:: with Gallery::

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Gallery;
 
 class GalleryController extends Controller
 {
     public function index($mangaid){
-        $generalData = DB::table('gallery_items')->where('id', $mangaid)->first();
+        $generalData = Gallery::where('id', $mangaid)->first();
 
         $tags = ["Action", "Sci-Fi", "Comedy", "Parody", "Super Power", "Supernatural", "Seinen"];
 
