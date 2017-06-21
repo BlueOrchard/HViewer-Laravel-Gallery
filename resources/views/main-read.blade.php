0b577cf3c0ceb285b7e1b6bb42c1c95e76b52769
BlueOrchard <josephrichey@me.com>
2017-06-21 00:22:30 -0400

    Added blade template files for /read route
    /read route is now fully functional albeit primitive. Also has links that lead back to the main gallery page for better navigation.

---
+++ resources/views/main-read.blade.php
@@ -0,0 +1,7 @@
+@include('required.header')
+    @include('layout.topnav')
+    <div class="content-without-nav">
+        @include('templates.gallery-read')
+        @include('layout.footer')
+    </div>
+@include('required.footer')
