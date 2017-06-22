a094416f89b92bf30df642a07b756affab7e9763
BlueOrchard <josephrichey@me.com>
2017-06-21 22:44:30 -0400

    Added all routes needed
    Basically set the foundation down for what needs to be done.

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,47 @@
 |
 */
 
+//Normal routes
+Route::get('/', function ($id) {
+    //TODO add homepage
+});
+
+//Individual manga/gallery routes
 Route::get('manga/{slug}', ['as' => 'slug', 'uses' => 'GalleryController@index']);
 Route::get('manga/{slug}/read', ['as' => 'slug', 'uses' => 'GalleryController@read']);
 
+//Archive routes for linear browsing
+Route::get('archive/', function ($id) {
+    //TODO add archive page
+});
+Route::get('archive/page/{num}', function ($id) {
+    //TODO add archive page and pagination
+});
+
+//Search and filter routes. Possibly merge these two routes together.
+Route::get('search/{$query}', function ($id) {
+    //TODO add search page
+});
+Route::get('filter/', function ($id) {
+    //TODO add filter page
+    //POSSIBLY MERGE WITH SEARCH PAGE (Browse)
+});
+
+Route::get('login/', function ($id) {
+    //TODO login and register 
+});
+
+//User profile routes. Start authentication middleware here.
+Route::get('profile/', function ($id) {
+    //TODO add profile page
+});
+Route::get('favorites/', function ($id) {
+    //TODO add favorites page
+});
+
+//Admin function routes. Add admin authentication middleware here.
+Route::get('manga-admin/', function ($id) {
+    //TODO add admin page for galleries/manga
+});
 Route::get('add-manga/new', ['uses' => 'UploadGalleryController@index']);
 Route::post('add-manga', ['uses' => 'UploadGalleryController@zipCreate']);
