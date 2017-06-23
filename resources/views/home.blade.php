0ec65704d5bf0c5f1c918659cd0585579d7a4992
BlueOrchard <josephrichey@me.com>
2017-06-23 11:12:45 -0400

    Minor styling changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,23 @@
     <div class="content-with-nav">
         @include('layout.leftnav')
         <div class="homepage">
-            <h1>Recently Added</h1>
-            <div class="holder">
-                @foreach($recentData as $item)
-                    @include('templates.nav-item')
-                @endforeach
-            </div>
+            <div class="leftside">
+                <h1>Recently Added</h1>
+                <div class="holder">
+                    @foreach($recentData as $item)
+                        @include('templates.nav-item')
+                    @endforeach
+                </div>
 
-            <h1>Check Out</h1>
-            <div class="holder">
-                @foreach($randomData as $item)
-                    @include('templates.nav-item')
-                @endforeach
+                <h1>Check Out</h1>
+                <div class="holder">
+                    @foreach($randomData as $item)
+                        @include('templates.nav-item')
+                    @endforeach
+                </div>
+            </div>
+            <div class="rightside">
+            
             </div>
         </div>
         @include('layout.footer')
