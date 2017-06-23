3afa9b1d6bbb785d258ce2459e9f5cbe02c82470
BlueOrchard <josephrichey@me.com>
2017-06-23 11:56:42 -0400

    Added more items to homepage
    Changed the styling of the individual galleries and added a "more information" sidebar.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,22 +4,27 @@
         @include('layout.leftnav')
         <div class="homepage">
             <div class="leftside">
-                <h1>Recently Added</h1>
-                <div class="holder">
-                    @foreach($recentData as $item)
-                        @include('templates.nav-item')
-                    @endforeach
-                </div>
+                <div class="moreitems">
+                    <h1>Recently Added</h1>
+                    <div class="holder">
+                        @foreach($recentData as $item)
+                            @include('templates.nav-item')
+                        @endforeach
+                    </div>
 
-                <h1>Check Out</h1>
-                <div class="holder">
-                    @foreach($randomData as $item)
-                        @include('templates.nav-item')
-                    @endforeach
+                    <h1>Check Out</h1>
+                    <div class="holder">
+                        @foreach($randomData as $item)
+                            @include('templates.nav-item')
+                        @endforeach
+                    </div>
                 </div>
             </div>
             <div class="rightside">
-            
+                <div class="readme">
+                    <span class="mdi mdi-book-open-variant"></span>
+                </div>
+                <p class="tooltip">Hover over an item to get more information on it.</p>
             </div>
         </div>
         @include('layout.footer')
