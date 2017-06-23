ec5ed8161ccadd3015a4b8b72ae6d0753733f97f
BlueOrchard <josephrichey@me.com>
2017-06-23 13:44:58 -0400

    Added "Hover for more information" script to homepage

--- resources/views/required/footer.blade.php
+++ resources/views/required/footer.blade.php
@@ -1,3 +1,21 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+        var timer;
+        $('.item-component').mouseenter(function(){
+            $(this).addClass('hovered');
+
+            var this_data = this;
+            timer = setTimeout(function(){
+                $('.rightside').html('<img src="'+$(this_data).data('img')+'">'+
+                        '<div class="entry"><span>Name</span><span>'+$(this_data).data('title')+'</span></div>'+
+                        '<div class="entry"><span>Series</span></span><span>'+$(this_data).data('series')+'</span></div>'+
+                        '<div class="entry"><span>Date Added</span><span>'+$(this_data).data('date')+'</span></div>'+
+                        '<a class="readmore" href="/manga/'+$(this_data).data('slug')+'">More Information</a>'
+                        );
+            }, 1000);
+        }).mouseleave(function() {
+            clearTimeout(timer);
+        });
+    </script>
     </body>
 </html>
--- resources/views/templates/nav-item.blade.php
+++ resources/views/templates/nav-item.blade.php
@@ -1,4 +1,10 @@
-<div class="item-component">
+<div class="item-component" 
+    data-slug="{{ $item->slug }}"
+    data-img="{{ $item->cover_photo }}"
+    data-date="{{ $item->created_at }}"
+    data-title="{{ $item->name }}"
+    data-series="{{ $item->series }}"
+    >
     <a href="/manga/{{ $item->slug }}">
         <img src="{{ url($item->cover_photo_thumb) }}">
         <p>{{ $item->name }}</p>
