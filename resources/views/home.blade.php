b09ea900a4327799d65b11743655614cc554f32d
BlueOrchard <josephrichey@me.com>
2017-06-23 09:52:31 -0400

    Added blade templates for home, browse, and archive

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@include('required.header')
+    @include('layout.topnav')
+    <div class="content-with-nav">
+        @include('layout.leftnav')
+
+        @include('layout.footer')
+    </div>
+@include('required.footer')
