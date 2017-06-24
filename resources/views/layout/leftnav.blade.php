0644074fd21d18bcb338a6de40f471d6f8f207fd
BlueOrchard <josephrichey@me.com>
2017-06-24 14:20:26 -0400

    Added links and CSS to left navigation menu

--- resources/views/layout/leftnav.blade.php
+++ resources/views/layout/leftnav.blade.php
@@ -1,7 +1,7 @@
 <div class="leftnav">
     <ul>
-        <li><span class="mdi mdi-home"></span>Home</li>
-        <li><span class="mdi mdi-heart"></span>Favorites</li>
-        <li><span class="mdi mdi-view-carousel"></span>Browse</li>
+        <li><a href="/"><span class="mdi mdi-home"></span>Home</a></li>
+        <li><a href="/browse"><span class="mdi mdi-view-carousel"></span>Browse</a></li>
+        <li><a href="/favorites"><span class="mdi mdi-heart"></span>Favorites</a></li>
     </ul>
 </div>
