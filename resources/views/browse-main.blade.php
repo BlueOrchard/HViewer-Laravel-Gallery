8461db558b95a9657dae4fdaf07051c4c1537dde
BlueOrchard <josephrichey@me.com>
2017-06-26 12:47:08 -0400

    Started work on /browse route

--- resources/views/browse-main.blade.php
+++ resources/views/browse-main.blade.php
@@ -2,7 +2,23 @@
     @include('layout.topnav')
     <div class="content-with-nav">
         @include('layout.leftnav')
-
+        <div class="browse">
+            <div class="leftside">
+                <div class="browseinfo">
+                    @include('templates.user.profile-snippet')
+                </div>
+                <div class="moreitems">
+                    <h1>Browse</h1>
+                </div>
+            </div>
+            <div class="rightside">
+                <div class="readme">
+                    <span class="mdi mdi-book-open-variant"></span>
+                </div>
+                <p class="tooltip">Hover over an item to get more information on it.</p>
+            </div>
+            <div class="rightsideloader"></div>
+        </div>
         @include('layout.footer')
     </div>
 @include('required.footer')
